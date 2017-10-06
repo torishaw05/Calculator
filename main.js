8e748093106832bce5acce0cd37d0abf52a550d2
Tori Shaw <torishaw@Toris-MacBook-Air.local>
2017-10-06 09:51:24 -0400

    calc

--- main.js
+++ main.js
@@ -1,28 +1,47 @@
-let calculator=document.getElementById('calculator');
-let button=document.getElementsByClassName('button');
-let result=document.getElementById('result');
-let array = [];
+// SET VARIABLES
+
+let buttons = document.getElementsByClassName('button');
+let clear = document.querySelector('#clearbtn');
+let decimal= false;
+let display = document.querySelector('#show_value');
+let val = '';
+let equals = document.getElementById('equals');
+let operation = document.getElementsByClassName('operation');
+// *******************BUTTONS FUNCTION************
 
 
-for(var i=0; i<button.length;i++){
-  button[i].addEventListener('click', callBack);
-}
-function callBack(e) {
-  result.textContent+=e.target.innerHTML;
-  if(e.target.innerHTML === "=" ) {
 
-    let sum = eval(array.join(""));
-    
-    array=[sum];
-    result.textContent=sum;
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function makeCalcWork() {
+      let buttonValue = this.innerHTML;
+        display.innerHTML += buttonValue;
+      }
+    }
 
-  }else if (e.target.innerHTML==='clear') {
-    array=[];
-    result.textContent= "";
-  }else {
-  array.push(e.target.innerHTML)
-console.log(array);
-}
+    clear.onclick = function clear() {
+        display.innerHTML = "";
+        val = "";
+    }
 
-}
-console.log(array)
+
+// *******************EQUALS FUNCTION**************
+
+
+    equals.onclick = function() {
+      val += display.innerHTML;
+      display.innerHTML = eval(val);
+      val = "";
+    }
+
+
+// ************PERATIONS FUNC****************
+
+
+    for (var i = 0; i < operation.length; i++) {
+      operation[i].onclick = function operators() {
+        val += display.innerHTML;
+        val += this.innerHTML;
+        display.innerHTML = "";
+        console.log(val);
+      }
+    }
